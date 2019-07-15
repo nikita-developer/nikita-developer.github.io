7da25b760f1788b375af74dca4fdb52785b7ce45
Nikita <n.kuznetsov@pitech.ru>
2019-07-15 09:45:04 +0300

    upgrade study 1.1

--- study/app/script/index.js
+++ study/app/script/index.js
@@ -1,56 +1,64 @@
 
-
 window.onload = function() {
-  var nomber,
-      count = 3;
-  nomber = Math.random() * (9 - 0 + 1);
-  nomber = Math.round(nomber);
-  console.log(nomber);
 
-  document.querySelector('button').onclick = function() {
-    var val = document.querySelector('input').value,
-        out = document.querySelector('.out'),
-        check = document.querySelector('.check');
+  var items = document.querySelectorAll('.item');
+  var bodyTouchScroll = document.querySelector('body');
+  var scrollMouse = 0;
+  var $touchStart;
 
-    val = parseInt(val);
-    count--;
-
-    function funReload() {
-      location.reload();
-    }
-
-    if (count > 0) {
-
-      if (val == nomber) {
-        out.innerHTML = 'Вы угадали';
-        setTimeout(funReload, 3000);
-
-      } else if (val > nomber) {
-        out.innerHTML = 'Ваше число больше';
-        if (count == 1) {
-          check.innerHTML = 'У вас ' + count + ' попытка';
-        } else if (count == 0) {
-          check.innerHTML = 'У вас ' + count + ' попыток';
-        } else {
-          check.innerHTML = 'У вас ' + count + ' попытки';
-        }
-      } else if (val < nomber) {
-        out.innerHTML = 'Ваше число меньше';
-        if (count == 1) {
-          check.innerHTML = 'У вас ' + count + ' попытка';
-        } else if (count == 0) {
-          check.innerHTML = 'У вас ' + count + ' попыток';
-        } else {
-          check.innerHTML = 'У вас ' + count + ' попытки';
-        }
-      };
-
+  document.onwheel = function (e) {
+    clearItems();
+    if (e.deltaY >= 0) {
+      nextItem();
     } else {
-      check.innerHTML = 'У вас ' + count + ' попыток';
-      console.log(check);
-      out.innerHTML = 'Вы проиграли, страница будет перезагружена';
-      setTimeout(funReload, 3000);
-    };
+      prevItem();
+    }
+  }
 
-  };
+  // Скролл на мобильниках
+  bodyTouchScroll.addEventListener('touchstart', function(e) {
+    $touchStart = e.changedTouches[0].screenY
+  })
+
+  bodyTouchScroll.addEventListener('touchmove', function(e) {
+    if ($touchStart + 20 < e.changedTouches[0].screenY) {
+      clearItems ();
+      prevItem();
+      $touchStart = e.changedTouches[0].screenY
+    } else if ($touchStart - 20 > e.changedTouches[0].screenY){
+      clearItems ();
+      nextItem();
+      $touchStart = e.changedTouches[0].screenY
+    }
+  })
+
+  // Функции
+  function clearItems () {
+    var heightPage = window.innerHeight
+    for (var i = 0; i < items.length; i++) {
+      items[i].style.zIndex = 0;
+      items[i].style.opacity = 0;
+    }
+    return
+  }
+
+  function nextItem () {
+    scrollMouse++
+    if (scrollMouse > items.length - 1) {
+      scrollMouse = 0;
+    }
+    items[scrollMouse].style.zIndex = 1
+    items[scrollMouse].style.opacity = 1
+    return
+  }
+
+  function prevItem () {
+    scrollMouse--
+    if (scrollMouse < 0) {
+      scrollMouse = items.length - 1
+    }
+    items[scrollMouse].style.zIndex = 1
+    items[scrollMouse].style.opacity = 1
+    return
+  }
 };
