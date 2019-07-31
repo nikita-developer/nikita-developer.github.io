55f5067d89f5d59b4f24aae73d9750436df67333
Nikita <n.kuznetsov@pitech.ru>
2019-07-31 16:23:13 +0300

    reset study

---
+++ study/app/script/autocomplete.ashx
@@ -0,0 +1,5 @@
+{
+query:'Li', // Оригинальный запрос
+suggestions:['Liberia','Libyan Arab Jamahiriya','Liechtenstein','Lithuania'], // Список подсказок
+data:['LR','LY','LI','LT'] // Необязательный параметр, список ключей вариантов подсказок. Используется в callback функции
+}
--- study/app/script/index.js
+++ study/app/script/index.js
@@ -1,64 +1,22 @@
 
-window.onload = function() {
+$(document).ready(function() {
+  var $language = ["Action Script", "C++/CLI", "C++", "ColdFusion", "Delphi",
+			      "Java", "Objective-C", "JavaScript", "Python", "Visual Basic"];
 
-  var items = document.querySelectorAll('.item');
-  var bodyTouchScroll = document.querySelector('body');
-  var scrollMouse = 0;
-  var $touchStart;
+    $('.language__addItem').autocomplete({
+        source: $language
+    })
 
-  document.onwheel = function (e) {
-    clearItems();
-    if (e.deltaY >= 0) {
-      nextItem();
-    } else {
-      prevItem();
-    }
-  }
+    $('button').click(function() {
+      var $val = $('.language__addItem').val();
+      if (!$val == '') {
+        $('.language').prepend('<div class="language__item">' + $val + ' <span class="close">x</span></div>');
+        $('.language__addItem').val('');
+      }
+    });
 
-  // Скролл на мобильниках
-  bodyTouchScroll.addEventListener('touchstart', function(e) {
-    $touchStart = e.changedTouches[0].screenY
-  })
+    $('.language').on('click', '.close',function () {
+      $(this).parent().remove();
+    })
 
-  bodyTouchScroll.addEventListener('touchmove', function(e) {
-    if ($touchStart + 20 < e.changedTouches[0].screenY) {
-      clearItems ();
-      prevItem();
-      $touchStart = e.changedTouches[0].screenY
-    } else if ($touchStart - 20 > e.changedTouches[0].screenY){
-      clearItems ();
-      nextItem();
-      $touchStart = e.changedTouches[0].screenY
-    }
-  })
-
-  // Функции
-  function clearItems () {
-    var heightPage = window.innerHeight
-    for (var i = 0; i < items.length; i++) {
-      items[i].style.zIndex = 0;
-      items[i].style.opacity = 0;
-    }
-    return
-  }
-
-  function nextItem () {
-    scrollMouse++
-    if (scrollMouse > items.length - 1) {
-      scrollMouse = 0;
-    }
-    items[scrollMouse].style.zIndex = 1
-    items[scrollMouse].style.opacity = 1
-    return
-  }
-
-  function prevItem () {
-    scrollMouse--
-    if (scrollMouse < 0) {
-      scrollMouse = items.length - 1
-    }
-    items[scrollMouse].style.zIndex = 1
-    items[scrollMouse].style.opacity = 1
-    return
-  }
-};
+});
