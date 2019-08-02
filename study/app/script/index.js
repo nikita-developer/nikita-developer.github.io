7bd3d3887d297c5a32d30eff60167033ff6c1544
Nikita <n.kuznetsov@pitech.ru>
2019-08-02 14:53:16 +0300

    reset study 1.3

--- study/app/script/index.js
+++ study/app/script/index.js
@@ -1,16 +1,4 @@
 
-$(document).ready(function() {
-  const image = document.getElementById('image');
-  const cropper = new Cropper(image, {
-    crop(event) {
-      var $top = event.detail.y;
-      var $right = event.detail.x + event.detail.width;
-      var $bottom = event.detail.y + event.detail.height;
-      var $left = event.detail.x;
-      console.log('left ' + $left + ' px');
-      console.log('top ' + $top + ' px');
-      console.log('right ' + $right + ' px');
-      console.log('bottom ' + $bottom + ' px');
-    },
-  });
-});
+// $(document).ready(function() {
+//
+// })
