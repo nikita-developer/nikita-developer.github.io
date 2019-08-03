a536700ee2d8c13fe87e89cb4355eb35839f9730
Nikita <n.kuznetsov@pitech.ru>
2019-08-03 14:57:42 +0300

    reset study 1.4

--- study/app/script/index.js
+++ study/app/script/index.js
@@ -1,4 +1,67 @@
 
-// $(document).ready(function() {
-//
-// })
+$(document).ready(function() {
+  $('.partnersSlide-js').owlCarousel({
+    loop: true,
+    doth: true,
+    responsive : {
+        0 : {
+          items: 2,
+        },
+
+        768 : {
+          items: 3,
+        }
+    }
+  })
+
+  $('.js-jobOpenings').owlCarousel({
+    doth: true,
+    loop: true,
+    items: 3,
+    stagePadding: 7,
+    margin: 30,
+    responsive : {
+        0 : {
+          items: 1,
+        },
+
+        768 : {
+          items: 3,
+        }
+    }
+  })
+
+  $('.js-articlesSlide').owlCarousel({
+    loop: true,
+    doth: true,
+    items: 3,
+    stagePadding: 15,
+    margin: 30,
+    responsive : {
+        0 : {
+          items: 1,
+        },
+
+        768 : {
+          items: 3,
+        }
+    }
+  })
+
+  $('.js-navOpen').click(function () {
+    $(this).toggleClass('navOpen_active');
+
+    if ($('.nav').hasClass('nav_active')) {
+      $('.nav').removeClass('nav_active')
+      $('.nav').stop().animate({opacity: "0"}, 300, function(){
+        $('.nav').css('display', 'none')
+      })
+    } else {
+      $('.nav').toggleClass('nav_active')
+      $('.nav').css('display', 'flex')
+      $('.nav').stop().animate({opacity: "1"}, 300)
+    }
+    $('html body').toggleClass('hideScroll')
+  })
+
+})
