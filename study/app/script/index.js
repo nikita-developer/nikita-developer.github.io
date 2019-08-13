b8b78e8ee0a5b4d3b5023ff1318ea6faa1d7b074
Nikita <nikitka.kuznetsov.1994@mail.ru>
2019-08-13 16:28:31 +0300

    reset study 2.6

--- study/app/script/index.js
+++ study/app/script/index.js
@@ -271,6 +271,9 @@ $(document).ready(function() {
     $(this).remove()
   })
 
+  $('.profileForm__phone').mask('+0-000-000-00-00')
 
-
+  $('.profileForm__openFio').change(function () {
+    $('.profileForm__fio').toggleClass('profileForm__fio_active')
+  })
 })
