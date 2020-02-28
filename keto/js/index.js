e15a32d13a65cea2ed383f83bbf26a13d9ed322f
Кузнецов Никита <n.kuznetsov@leadtop.local>
2020-02-28 13:54:32 +0300

    keto upgrade 1.7

--- keto/js/index.js
+++ keto/js/index.js
@@ -3,18 +3,18 @@ $(document).ready(function() {
     $('.js-join__slide').owlCarousel({
         items: 1,
         loop: true,
-        // autoplay:true,
-        // autoplayTimeout:3000,
-        // autoplayHoverPause:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
         nav: true
     });
 
     $('.js-commit__slide').owlCarousel({
         items: 1,
         loop: true,
-        // autoplay:true,
-        // autoplayTimeout:3000,
-        // autoplayHoverPause:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
         responsive : {
             0 : {
                 nav: false,
@@ -25,64 +25,97 @@ $(document).ready(function() {
         }
     });
 
+// запуск таймеров
     var clock;
-    var clock2;
+    var futureDate = new Date("Mart 02, 2020 0:00 PM EDT");
+    var currentDate = new Date();
+    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
-    clock = $('.js-timerOne').FlipClock({
-        clockFace: 'DailyCounter',
-        autoStart: false,
-        countdown: true,
-        language: 'ru'
-    });
-
-    clock2 = $('.js-timerTwo').FlipClock({
-        clockFace: 'DailyCounter',
-        autoStart: false,
-        countdown: true,
-        language: 'ru'
-    });
-
-    function getSecondsToTomorrow() {
-        var now = new Date();
-        // завтрашняя дата
-        var tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 3);
-        var diff = tomorrow - now;
-        return Math.round(diff / 1000); // преобразуем в секунды
+    function dayDiff(first, second) {
+        return (second - first) / (1000 * 60 * 60 * 24);
     }
+    if (dayDiff(currentDate, futureDate) < 100) {
+        $('.js-timerOne').addClass('twoDayDigits');
+    } else {
+        $('.js-timerOne').addClass('threeDayDigits');
+    }
+    if (diff < 0) {
+        diff = 0;
+    }
+    clock = $('.js-timerOne').FlipClock(diff, {
+        clockFace: 'DailyCounter',
+        language: 'ru',
+        countdown: true
+    });
 
-    var $result = getSecondsToTomorrow();
+    var clock2;
+    var futureDate = new Date("Mart 02, 2020 0:00 PM EDT");
+    var currentDate = new Date();
+    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
-    clock.setTime($result);      //Устанавливаем нужное время в секундах
-    clock.setCountdown(true);     //Устанавливаем отсчет назад
-    clock.start();                //Запускаем отсчет
-    clock2.setTime($result);      //Устанавливаем нужное время в секундах
-    clock2.setCountdown(true);   //Устанавливаем отсчет назад
-    clock2.start();              //Запускаем отсчет
+    function dayDiff(first, second) {
+        return (second - first) / (1000 * 60 * 60 * 24);
+    }
+    if (dayDiff(currentDate, futureDate) < 100) {
+        $('.js-timerTwo').addClass('twoDayDigits');
+    } else {
+        $('.js-timerTwo').addClass('threeDayDigits');
+    }
+    if (diff < 0) {
+        diff = 0;
+    }
+    clock2 = $('.js-timerTwo').FlipClock(diff, {
+        clockFace: 'DailyCounter',
+        language: 'ru',
+        countdown: true
+    });
+// запуск таймеров конец
 
-    $('.js-openPopap').click(function () {
-        $('.js-popap').fadeIn();
-        $('body, html').addClass('hide_body');
-    })
-
-    $('.section__close').click(function () {
-        $('.js-popap').fadeOut();
-        $('body, html').removeClass('hide_body');
+    $('.js-openPopap').click(function (e) {
+        if($('.form').valid()) {
+            $('.js-popap').fadeIn();
+            $('body, html').addClass('hide_body');
+            $('.form').trigger('reset');
+            $('.form__field, .form__select').removeClass('valid');
+            e.preventDefault();
+            console.log(2);
+        }
     })
 
     $(document).mouseup(function (e){
 		var div = $('.js-popap .container');
 		if (!div.is(e.target) && div.has(e.target).length === 0) {
-            $('body, html').removeClass('hide_body');
 			$('.js-popap').fadeOut();
+            $('body, html').removeClass('hide_body');
 		}
 	});
 
-    $('.purchase__btn').click(function (){
-        $('.purchase__btn').text('SELECTED PACKAGE!');
+    $('.purchase__item').click(function (){
+        var price = $('.js-price');
+        var priceRetail = $('.js-priceRetail');
+        var priceSave = $('.js-priceSave b');
+
+        if($(this).index() == 0) {
+            price.text('$136')
+            priceRetail.text('Retail: $264')
+            priceSave.text('You Save: $103')
+            console.log(1);
+        } else if ($(this).index() == 1) {
+            price.text('$117')
+            priceRetail.text('Retail: $132')
+            priceSave.text('You Save: $44')
+            console.log(2);
+        } else {
+            price.text('$5')
+            priceRetail.text('Retail: $44')
+            priceSave.text('You Save: $5')
+            console.log(3);
+        }
+
+        $(this).find('.purchase__btn').text('SELECTED PACKAGE!');
         $('.purchase__item').removeClass('purchase__item_active');
-        $(this).text('SELECTED!').parents('.purchase__item').addClass('purchase__item_active');
-        var price = $(this).prev().text();
-        $('.js-price').text(price);
+        $('.purchase__item').find('.purchase__btn').text('SELECTED PACKAGE!');
+        $(this).addClass('purchase__item_active').find('.purchase__btn').text('SELECTED!');
     })
 
     $('.form').validate({
@@ -111,6 +144,9 @@ $(document).ready(function() {
                 minlength: 3,
                 maxlength: 3,
                 number: true
+            },
+            date:{
+                number: true
             }
         }
     });
@@ -128,25 +164,27 @@ $(document).ready(function() {
         }
     });
 
-    $(".form").submit(function () {
-        var formID = $(this);
-        $.ajax({
-            type: "POST",
-            url: 'mail.php',
-            data: formID.serialize(),
-            success: function () {
-                showSucses ()
-            },
-            error: function () {
-                showError()
-            }
-        });
-        return false;
-    });
+    $('.pay__submit').click(function (e) {
+        if($('.pay__form').valid()) {
+            $('.js-popap').fadeOut();
+            $('body, html').removeClass('hide_body');
+            $('.pay__form').trigger('reset');
+            $('.pay__field, .pay__select').removeClass('valid');
+            e.preventDefault();
+            showError ();
+        }
+    })
+
+    $('.section__close').click(function () {
+        $('.js-popap').fadeOut();
+        $('body, html').removeClass('hide_body');
+        $('.pay__form').trigger('reset');
+        $('.pay__field, .pay__select').removeClass('valid');
+    })
 
     function showError () {
         $('.messageError').fadeIn();
-        setTimeout(hideError, 3000);
+        setTimeout(hideError, 5000);
     }
 
     function hideError () {
@@ -155,24 +193,22 @@ $(document).ready(function() {
 
     function showSucses () {
         $('.messageSuccess').fadeIn();
-        setTimeout(hideError, 3000);
+        setTimeout(hideError, 5000);
     }
 
     function hideSucses () {
         $('.messageSuccess').fadeOut();
     }
 
-    $('.form__field').on("blur", function(){
-        if($('.form').valid()) {
-            $('.form__btn').prop('disabled', false);
-        } else {
-            $('.form__btn').prop('disabled', true);
-        }
+    // Cкролл к форме
+    $('.js-scrollTop').click(function () {
+        var destination = $('.form').offset().top;
+        $('html, body').animate({ scrollTop: destination }, 1100);
     });
 
     // Всплывающие волокна
-    $(document).mouseup(function (e){
-		var div = $('.popup .container');
+    $($('.popup')).click(function (e){
+		var div = $('.popup .popup__box');
 		if (!div.is(e.target) && div.has(e.target).length === 0) {
             $('body, html').removeClass('hide_body');
 			$('.popup').fadeOut();
