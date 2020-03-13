124b8d8a542ff94a864a91925d7e510e9ddc5e27
Кузнецов Никита <n.kuznetsov@leadtop.local>
2020-03-13 14:19:06 +0300

    keto project ignatov

--- keto/js/index.js
+++ keto/js/index.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
 
 // запуск таймеров
     var clock;
-    var futureDate = new Date("Mart 02, 2020 0:00 PM EDT");
+    var futureDate = new Date("Mart 17, 2020 0:00 PM EDT");
     var currentDate = new Date();
     var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
@@ -35,52 +35,19 @@ $(document).ready(function() {
         return (second - first) / (1000 * 60 * 60 * 24);
     }
     if (dayDiff(currentDate, futureDate) < 100) {
-        $('.js-timerOne').addClass('twoDayDigits');
+        $('.js-timer').addClass('twoDayDigits');
     } else {
-        $('.js-timerOne').addClass('threeDayDigits');
+        $('.js-timer').addClass('threeDayDigits');
     }
     if (diff < 0) {
         diff = 0;
     }
-    clock = $('.js-timerOne').FlipClock(diff, {
-        clockFace: 'DailyCounter',
-        language: 'ru',
-        countdown: true
-    });
-
-    var clock2;
-    var futureDate = new Date("Mart 02, 2020 0:00 PM EDT");
-    var currentDate = new Date();
-    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
-
-    function dayDiff(first, second) {
-        return (second - first) / (1000 * 60 * 60 * 24);
-    }
-    if (dayDiff(currentDate, futureDate) < 100) {
-        $('.js-timerTwo').addClass('twoDayDigits');
-    } else {
-        $('.js-timerTwo').addClass('threeDayDigits');
-    }
-    if (diff < 0) {
-        diff = 0;
-    }
-    clock2 = $('.js-timerTwo').FlipClock(diff, {
+    clock = $('.js-timer').FlipClock(diff, {
         clockFace: 'DailyCounter',
         language: 'ru',
         countdown: true
     });
 // запуск таймеров конец
-
-    $('.js-openPopap').click(function (e) {
-        if($('.form').valid()) {
-            $('.js-popap').fadeIn();
-            $('body, html').addClass('hide_body');
-            $('.form').trigger('reset');
-            $('.form__field, .form__select').removeClass('valid');
-            e.preventDefault();
-            console.log(2);
-        }
-    })
 
     $(document).mouseup(function (e){
 		var div = $('.js-popap .container');
@@ -90,127 +57,13 @@ $(document).ready(function() {
 		}
 	});
 
-    $('.purchase__item').click(function (){
-        var priceType = $('.js-priceDlivery');
-        var price = $('.js-price');
-        var priceRetail = $('.js-priceRetail');
-        var priceSave = $('.js-priceSave b');
-
-        if($(this).index() == 0) {
-            priceType.text('€0')
-            price.text('€136')
-            priceRetail.text('Retail: €354')
-            priceSave.text('You Save: €218')
-            console.log(1);
-        } else if ($(this).index() == 1) {
-            priceType.text('€0')
-            price.text('€117')
-            priceRetail.text('Retail: €236')
-            priceSave.text('You Save: €119')
-            console.log(2);
-        } else {
-            priceType.text('€5')
-            price.text('€44')
-            priceRetail.text('Retail: €59')
-            priceSave.text('You Save: €20')
-            console.log(3);
-        }
-
-        $(this).find('.purchase__btn').text('SELECTED PACKAGE!');
-        $('.purchase__item').removeClass('purchase__item_active');
-        $('.purchase__item').find('.purchase__btn').text('SELECTED PACKAGE!');
-        $(this).addClass('purchase__item_active').find('.purchase__btn').text('SELECTED!');
-    })
-
-    $('.form').validate({
-        rules:{
-            tel:{
-                minlength: 6,
-                maxlength: 18,
-                number: true
-            },
-            ZipCode:{
-                minlength: 2,
-                maxlength: 18,
-                number: true
-            }
-        }
-    });
-
-    $('.pay__form').validate({
-        rules:{
-            card:{
-                minlength: 16,
-                maxlength: 16,
-                number: true
-            },
-            cvv:{
-                minlength: 3,
-                maxlength: 3,
-                number: true
-            },
-            date:{
-                number: true
-            }
-        }
-    });
-
-    $('.contact__form').validate({
-        rules:{
-            email:{
-                email: true,
-                required: true
-            },
-            tel:{
-                required: true,
-                number: true
-            }
-        }
-    });
-
-    $('.pay__submit').click(function (e) {
-        if($('.pay__form').valid()) {
-            $('.js-popap').fadeOut();
-            $('body, html').removeClass('hide_body');
-            $('.pay__form').trigger('reset');
-            $('.pay__field, .pay__select').removeClass('valid');
-            e.preventDefault();
-            showError ();
-        }
-    })
-
-    $('.section__close').click(function () {
-        $('.js-popap').fadeOut();
-        $('body, html').removeClass('hide_body');
-        $('.pay__form').trigger('reset');
-        $('.pay__field, .pay__select').removeClass('valid');
-    })
-
-    function showError () {
-        $('.messageError').fadeIn();
-        setTimeout(hideError, 7000);
-    }
-
-    function hideError () {
-        $('.messageError').fadeOut();
-    }
-
-    function showSucses () {
-        $('.messageSuccess').fadeIn();
-        setTimeout(hideError, 7000);
-    }
-
-    function hideSucses () {
-        $('.messageSuccess').fadeOut();
-    }
-
     // Cкролл к форме
     $('.js-scrollTop').click(function () {
         var destination = $('.form').offset().top;
         $('html, body').animate({ scrollTop: destination }, 1100);
     });
 
-    // Всплывающие волокна
+    // Всплывающие окна
     $($('.popup')).click(function (e){
 		var div = $('.popup .popup__box');
 		if (!div.is(e.target) && div.has(e.target).length === 0) {
