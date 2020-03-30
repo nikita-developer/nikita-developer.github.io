84a5b716a8e4c42d847162a8ccfc0331d7a63d10
Nikita <nikitka.kuznetsov.1994@mail.ru>
2020-03-30 17:13:34 +0300

    canabis add smoke

--- canabis/js/index.js
+++ canabis/js/index.js
@@ -1,4 +1,38 @@
 
 $(document).ready(function() {
-    $('#date').data('datepicker')
+    $('#date').data('datepicker');
+    Start();
+
+    function Start() {
+        var h = 0;
+        var m = 12;
+        var s = 0;
+
+        var timerId = setTimeout(function tick() {
+            if (s != 0) {
+                s = s - 1;
+            } else {
+                if (m != 0) {
+                    m = m - 1;
+                    s = 59;
+                } else {
+                    if (h != 0) {
+                        h = h - 1;
+                        m = 59;
+                        s = 59;
+                    } else {
+                        return
+                    }
+                }
+            }
+            $('.form__count-item:first-child span:first-child').text(h);
+            $('.form__count-item:nth-child(2) span:first-child').text(m);
+            $('.form__count-item:last-child span:first-child').text(s);
+            timerId = setTimeout(tick, 1000);
+        }, 1000)
+    }
+
+    /*init smoke*/
+    init()
+
 });
