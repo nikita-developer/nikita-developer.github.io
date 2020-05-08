475f92415dfbb93ce75a1fa47f3f24b92027222f
Nikita <nikitka.kuznetsov.1994@mail.ru>
2020-05-08 15:38:08 +0300

    moneyAmulet правки 1.0

--- moneyAmulet/js/index.js
+++ moneyAmulet/js/index.js
@@ -7,12 +7,4 @@ $(document).ready(function() {
             $(this).removeClass('form__field_active')
         }
     })
-
-    $('.commit__description').liTextLength({
-        length: 150,
-        afterLength: '...',
-        fullText:true,
-        moreText: 'Подробнее',
-        lessText: 'Скрыть'
-    });
 });
