a09b19a68134f87f610bed69c3588157ad9900d3
Nikita <nikitka.kuznetsov.1994@mail.ru>
2020-05-12 17:08:03 +0300

    moneyAmulet правки 1.4

--- moneyAmulet/js/index.js
+++ moneyAmulet/js/index.js
@@ -7,4 +7,27 @@ $(document).ready(function() {
             $(this).removeClass('form__field_active')
         }
     })
+
+    $('.js-commit__slide').owlCarousel({
+        loop: true,
+        responsive : {
+            320 : {
+                items: 1,
+                autoHeight: true,
+                margin: 40
+            },
+
+            768 : {
+                items: 2,
+                margin: 40
+            },
+
+            960 : {
+                items: 3,
+                autoHeight: false,
+                margin: 30
+            }
+        }
+    });
+
 });
