c17c2f493be27cc52cbe2eff05cf7696234b7c3d
Nikita <nikitka.kuznetsov.1994@mail.ru>
2020-05-20 14:03:57 +0300

    moneyAmulet-3 1.2

--- moneyAmulet-3/js/index.js
+++ moneyAmulet-3/js/index.js
@@ -27,4 +27,14 @@ $(document).ready(function() {
             }
         }
     });
+
+    $('.secret__btn').on( 'click', function() {
+        var dest = $('.face__box_form');
+        if(dest !== undefined && dest !== '') {
+            $('html').animate({
+                scrollTop: $(dest).offset().top
+            }, 500);
+        }
+        return false;
+    });
 });
