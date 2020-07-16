ea3067133ded25d95570c16de86cf50b0d6911f0
Nikita <nikitka.kuznetsov.1994@mail.ru>
2020-07-16 17:26:14 +0300

    cibershop

--- cibershop/js/index.js
+++ cibershop/js/index.js
@@ -4,4 +4,23 @@ $(document).ready(function() {
         $('.header__nav-body').toggleClass('header__nav-body_active');
         $(this).toggleClass('header__nav-burger_active');
     })
+
+    $('.face').owlCarousel({
+        loop: true,
+        margin: 0,
+        autoHeight: true,
+        items: 1
+    });
+
+    $(window).scroll(function(){
+        var height = $('.header__head').outerHeight();
+        var navHeight = $('.header__nav').outerHeight();
+        if ($(this).scrollTop() >= height) {
+            $('.header__nav').addClass('header__nav_mobile');
+            $('.section_face').css('margin-top', navHeight);
+        } else {
+            $('.header__nav').removeClass('header__nav_mobile');
+            $('.section_face').css('margin-top', 0);
+        }
+    });
 });
